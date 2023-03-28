4e39c53c8fc4c555dca5872f783b248ab7652e39
gumarengo <gu_marengo@hotmail.com>
2023-03-28 14:42:32 -0300

    Adds image to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import "./index.scss";
 import Animation from "../Animation/Animation";
-import portfolioData from "../../data/portfolio.json";
+import { portfolio } from "../../data/portfolio";
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
-  console.log(portfolioData);
+  console.log(portfolio);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -18,6 +18,54 @@ const Portfolio = () => {
     };
   }, []);
 
+  //   const renderPortfolio = (portfolio) => {
+  //     return (
+  //       <div className="images-container">
+  //         {portfolio.map((port, i) => {
+  //           return (
+  //             <div className="image-box" key={i}>
+  //               <img
+  //                 src={port.cover}
+  //                 alt="project image"
+  //                 className="portfolio-image"
+  //               />
+  //             </div>
+  //           );
+  //         })}
+  //       </div>
+  //     );
+  //   };
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              {port.description}
+              <br />
+              <img
+                src={port.cover}
+                // "https://t.ctcdn.com.br/essK16aBUDd_65hp5umT3aMn_i8=/400x400/smart/filters:format(webp)/i606944.png"
+                className="portfolio-image"
+                alt="portfolio"
+              />
+
+              {/* <div className="content">
+                                <p className="title">{port.name}</p>
+                                <h4 className="description">{port.description}</h4>
+                                <button
+                                    className="btn"
+                                    onClick={() => window.open(port.url)}
+                                >View</button>
+                            </div> */}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <>
       <div className="container portfolio-page">
@@ -28,7 +76,8 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
-        <div>BATATA</div>
+        <div>{renderPortfolio(portfolio)}</div>
+        <h1>GUSTAVO TESTE</h1>
       </div>
       <Loader type="pacman" />
     </>
---
+++ src/data/portfolio.js
@@ -0,0 +1,22 @@
+export const portfolio = [
+  {
+    cover: require("../assests/portfolio-img/one.png"),
+    title: "GGWP",
+    description: "React, JSX, Tailwind, Jest",
+    url: "http://google.com",
+  },
+  {
+    cover:
+      "https://t.ctcdn.com.br/essK16aBUDd_65hp5umT3aMn_i8=/400x400/smart/filters:format(webp)/i606944.png",
+    title: "GGWP",
+    description: "React, JSX, Tailwind, Jest",
+    url: "http://google.com",
+  },
+  {
+    cover:
+      "https://t.ctcdn.com.br/essK16aBUDd_65hp5umT3aMn_i8=/400x400/smart/filters:format(webp)/i606944.png",
+    title: "GGWP",
+    description: "React, JSX, Tailwind, Jest",
+    url: "http://google.com",
+  },
+];
