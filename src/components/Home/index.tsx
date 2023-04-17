49c1699725f290237742f28734c6363b0ce686b7
gumarengo <gu_marengo@hotmail.com>
2023-04-17 09:36:27 -0300

    Fix: Adjusts Particles speed

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -53,12 +53,12 @@ const Home = () => {
           init={particlesInit}
           loaded={particlesLoaded}
           options={{
-            background: {
-              color: {
-                value: "#022c43",
-              },
-            },
-            fpsLimit: 120,
+            // background: {
+            //   color: {
+            //     value: "#022c43",
+            //   },
+            // },
+            // fpsLimit: 120,
             interactivity: {
               events: {
                 onClick: {
@@ -69,7 +69,7 @@ const Home = () => {
                   enable: true,
                   mode: "repulse",
                 },
-                resize: true,
+                resize: false,
               },
               modes: {
                 push: {
@@ -81,47 +81,110 @@ const Home = () => {
                 },
               },
             },
+            // particles: {
+            //   color: {
+            //     value: "#ffffff",
+            //   },
+            //   links: {
+            //     color: "#ffffff",
+            //     distance: 150,
+            //     enable: true,
+            //     opacity: 0.5,
+            //     width: 1,
+            //   },
+            //   collisions: {
+            //     enable: true,
+            //   },
+            //   move: {
+            //     enable: true,
+            //     outModes: {
+            //       default: "bounce",
+            //     },
+            //     random: false,
+            //     speed: 6,
+            //     straight: false,
+            //   },
+            //   number: {
+            //     density: {
+            //       enable: true,
+            //       area: 800,
+            //     },
+            //     value: 80,
+            //   },
+            //   opacity: {
+            //     value: 0.5,
+            //   },
+            //   shape: {
+            //     type: "circle",
+            //   },
+            //   size: {
+            //     value: { min: 1, max: 5 },
+            //   },
+            // },
+            // detectRetina: true,
+            fps_limit: 60,
             particles: {
-              color: {
-                value: "#ffffff",
-              },
-              links: {
+              color: { value: "#ffffff" },
+              line_linked: {
                 color: "#ffffff",
                 distance: 150,
                 enable: true,
-                opacity: 0.5,
+                opacity: 0.4,
                 width: 1,
               },
-              collisions: {
-                enable: true,
-              },
               move: {
+                attract: { enable: false, rotateX: 600, rotateY: 1200 },
+                bounce: false,
+                direction: "none",
                 enable: true,
-                outModes: {
-                  default: "bounce",
-                },
+                out_mode: "out",
                 random: false,
-                speed: 6,
+                speed: 2,
                 straight: false,
               },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 80,
-              },
+              number: { density: { enable: true, value_area: 800 }, value: 80 },
               opacity: {
+                anim: {
+                  enable: false,
+                  opacity_min: 0.1,
+                  speed: 1,
+                  sync: false,
+                },
+                random: false,
                 value: 0.5,
               },
               shape: {
+                character: {
+                  fill: false,
+                  font: "Verdana",
+                  style: "",
+                  value: "*",
+                  weight: "400",
+                },
+                image: {
+                  height: 100,
+                  replace_color: true,
+                  src: "images/github.svg",
+                  width: 100,
+                },
+                polygon: { nb_sides: 5 },
+                stroke: { color: "#000000", width: 0 },
                 type: "circle",
               },
               size: {
-                value: { min: 1, max: 5 },
+                anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
+                random: true,
+                value: 5,
               },
             },
-            detectRetina: true,
+            polygon: {
+              draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
+              move: { radius: 10 },
+              scale: 1,
+              type: "none",
+              url: "",
+            },
+            retina_detect: true,
           }}
         />
         <div className="text-zone">
